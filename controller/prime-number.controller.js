e5eb528c804c28d1ff68a1e80ffbb78551bde32c
Cristian Gómez <cristiang.contacto@gmail.com>
2021-03-21 23:04:46 -0300

    [controller] ADD added controller as a one more layer

---
+++ controller/prime-number.controller.js
@@ -0,0 +1,19 @@
+const { getPrimeNumbersByLimitNum } = require('../services/prime-number.service')
+const { defaultMsgErr } = require('../utils/constants');
+
+const primeNumberController = async (req, res) => {
+   try {
+
+      const { limitNumber } = req.body
+      const primeNumbers = getPrimeNumbersByLimitNum(limitNumber);
+      res.status(200).json({ primeNumbers })
+
+   } catch (err) {
+
+      console.error(err)
+      res.status(err.code || 500).json({ message: err.message || defaultMsgErr })
+
+   }
+}
+
+module.exports = { primeNumberController }
